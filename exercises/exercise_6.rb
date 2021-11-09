053ce2a500832a8e01564c6d8fc357783d175486
Hasan Lam <hcb.lam@gmail.com>
2021-11-09 19:20:14 +0000

    Complete Exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,21 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Bob", last_name: "Smith", hourly_rate: 40)
+@store2.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 50)
+@store2.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 50)
+
+# store1_employees = Employee.where(store_id: 1)
+
+# store1_employees.each do |employee|
+#   puts "#{employee.first_name} #{employee.last_name} works at store 1."
+# end
